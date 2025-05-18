d77ce5a8cf12c3e654c2a123d5744e9dfa35ab9b
sallapauliina <salla.p.lyytikainen@aalto.fi>
2025-05-18 15:05:56 +0300

    push

--- index.js
+++ index.js
@@ -1,38 +1,38 @@
-const morgan = require("morgan");
-const cors = require("cors");
+const morgan = require('morgan')
+const cors = require('cors')
 
-require("dotenv").config();
-const express = require("express");
+require('dotenv').config()
+const express = require('express')
 
-const Note = require("./models/person");
-const app = express();
+const Note = require('./models/person')
+const app = express()
 
 const requestLogger = (request, response, next) => {
-  next();
-};
+  next()
+}
 
 const errorHandler = (error, request, response, next) => {
-  console.error(error.message);
+  console.error(error.message)
 
-  if (error.name === "CastError") {
-    return response.status(400).send({ error: "malformatted id" });
-  } else if (error.name === "ValidationError") {
-    return response.status(400).json({ error: error.message });
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
   }
-  next(error);
-};
+  next(error)
+}
 
-app.use(cors());
-app.use(express.static("dist"));
-app.use(express.json());
-app.use(requestLogger);
+app.use(cors())
+app.use(express.static('dist'))
+app.use(express.json())
+app.use(requestLogger)
 
-const mongoose = require("mongoose");
+// const mongoose = require('mongoose')
 
-const password = process.argv[2];
+// const password = process.argv[2]
 
-const name = process.argv[3];
-const number = process.argv[4];
+// const name = process.argv[3]
+// const number = process.argv[4]
 
 // const url = `mongodb+srv://sallalyyti:${password}@cluster0.htfc4fh.mongodb.net/noteApp?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -80,17 +80,17 @@ const number = process.argv[4];
 //   },
 // ];
 
-app.use(express.json());
+app.use(express.json())
 
-morgan.token("body", (request) => JSON.stringify(request.body));
+morgan.token('body', (request) => JSON.stringify(request.body))
 
 app.use(
-  morgan(":method :url :status :res[content-length] - :response-time ms :body")
-);
+  morgan(':method :url :status :res[content-length] - :response-time ms :body')
+)
 
-app.get("/", (request, response) => {
-  response.send("<h1>Hello World!!!</h1>");
-});
+app.get('/', (request, response) => {
+  response.send('<h1>Hello World!!!</h1>')
+})
 
 // app.get("/api/notes", (request, response) => {
 //   response.json(notes);
@@ -100,12 +100,12 @@ app.get("/", (request, response) => {
 //   response.json(persons);
 // });
 
-app.get("/info", (request, response) => {
-  response.send(
-    `<p>Phonebook has info for ${persons.length} people.</p>
-    <p>${new Date()}</p>`
-  );
-});
+app.get('/info', (request, response) => {
+  Note.find({}).then((persons) => {
+    response.send(`<p>Phonebook has info for ${persons.length} people.</p>
+    <p>${new Date()}</p>`)
+  })
+})
 
 // app.get("/api/persons/:id", (request, response) => {
 //   const id = Number(request.params.id);
@@ -117,17 +117,17 @@ app.get("/info", (request, response) => {
 //   }
 // });
 
-app.get("/api/persons/:id", (request, response, next) => {
+app.get('/api/persons/:id', (request, response, next) => {
   Note.findById(request.params.id)
     .then((note) => {
       if (note) {
-        response.json(note);
+        response.json(note)
       } else {
-        response.status(404).end();
+        response.status(404).end()
       }
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
 // const generateId = () => {
 //   const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
@@ -165,20 +165,20 @@ app.get("/api/persons/:id", (request, response, next) => {
 //   }
 // });
 
-app.get("/api/persons", (request, response) => {
+app.get('/api/persons', (request, response) => {
   Note.find({}).then((persons) => {
-    response.json(persons);
-  });
-});
+    response.json(persons)
+  })
+})
 
-app.delete("/api/persons/:id", (request, response) => {
-  const id = request.params.id;
+app.delete('/api/persons/:id', (request, response, next) => {
+  const id = request.params.id
   Note.findByIdAndDelete(id)
-    .then((result) => {
-      response.status(204).end();
+    .then(() => {
+      response.status(204).end()
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
 // app.delete("/api/persons/:id", (request, response) => {
 //   const id = Number(request.params.id);
@@ -213,78 +213,70 @@ app.delete("/api/persons/:id", (request, response) => {
 //   response.json(newPerson);
 // });
 
-const phonePattern = /^[0-9]{2,3}-[0-9]{5,}$/;
+const phonePattern = /^[0-9]{2,3}-[0-9]{5,}$/
 
-app.post("/api/persons", (request, response, next) => {
-  const body = request.body;
+app.post('/api/persons', (request, response, next) => {
+  const body = request.body
 
   if (!body.name || !body.number) {
     return response.status(400).json({
-      error: "Name or number is missing.",
-    });
+      error: 'Name or number is missing.',
+    })
   }
 
   if (body.number.length < 8) {
     return response.status(400).json({
-      error: "Number is too short.",
-    });
+      error: 'Number is too short.',
+    })
   }
 
   if (!phonePattern.test(body.number)) {
     return response.status(400).json({
-      error: "Invalid phone number.",
-    });
+      error: 'Invalid phone number.',
+    })
   }
 
   const newPerson = new Note({
     name: body.name,
     number: body.number,
-  });
+  })
 
   newPerson
     .save()
     .then((savedPerson) => {
-      response.json(savedPerson);
+      response.json(savedPerson)
     })
     .catch((error) => {
-      next(error);
-    });
-  // .catch(next);
-  // .catch((error) => {
-  //   console.log(error);
-  //   console.error("Error saving person:", error);
-  //   response.status(400).json({
-  //     error: "Name or number is missing.",
-  //   });
-  // });
-});
+      next(error)
+    })
+})
 
-app.put("/api/persons/:id", (request, response, next) => {
-  const { name, number } = request.body;
+app.put('/api/persons/:id', (request, response, next) => {
+  const { name, number } = request.body
   Note.findById(request.params.id)
     .then((note) => {
       if (note) {
-        note.name = name;
-        note.number = number;
+        note.name = name
+        note.number = number
         return note.save().then((updatedNote) => {
-          response.json(updatedNote);
-        });
+          response.json(updatedNote)
+        })
       } else {
-        response.status(404).end();
+        response.status(404).end()
       }
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
-const unkonwnEndpoint = (request, response) => {
-  response.status(404).send({ error: "unknown endpoint" });
-};
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
 
-app.use(unkonwnEndpoint);
+app.use(unknownEndpoint)
 
-app.use(errorHandler);
+app.use(errorHandler)
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+  console.log(`Server running on port ${PORT}`)
+})
--- models/person.js
+++ models/person.js
@@ -1,24 +1,24 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose')
 
-mongoose.set("strictQuery", false);
+mongoose.set('strictQuery', false)
 
-const url = process.env.MONGODB_URI;
+const url = process.env.MONGODB_URI
 
-console.log("connecting to", url);
+console.log('connecting to', url)
 mongoose
   .connect(url)
 
-  .then((result) => {
-    console.log("connected to MongoDB");
+  .then(() => {
+    console.log('connected to MongoDB')
   })
   .catch((error) => {
-    console.log("error connecting to MongoDB:", error.message);
-  });
+    console.log('error connecting to MongoDB:', error.message)
+  })
 
 const noteSchema = new mongoose.Schema({
   name: {
     type: String,
-    minlength: [3, "Name must be at least 3 characters."],
+    minlength: [3, 'Name must be at least 3 characters.'],
     required: true,
   },
   //   number: {
@@ -31,20 +31,20 @@ const noteSchema = new mongoose.Schema({
     minlength: 8,
     validate: {
       validator: function (v) {
-        return /^[0-9]{2,3}-[0-9]{5,}$/.test(v);
+        return /^[0-9]{2,3}-[0-9]{5,}$/.test(v)
       },
       message: (props) => `${props.value} is not a valid phone number!`,
     },
     required: true,
   },
-});
+})
 
-noteSchema.set("toJSON", {
+noteSchema.set('toJSON', {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
   },
-});
+})
 
-module.exports = mongoose.model("Note", noteSchema);
+module.exports = mongoose.model('Note', noteSchema)
